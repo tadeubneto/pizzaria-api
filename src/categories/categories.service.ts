6dda6a2fd7a91e7a6b296c44614540f2b3a399ef
Tadeu <tadeubranconeto@gmail.com>
2024-12-18 11:38:36 -0300

    feat: create service pizza

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(private prisma: PrismaService){}
+
+
+    async create(createCategoryDto: CreateCategoryDto) {
+        return this.prisma.category.create({
+            data: createCategoryDto,
+        })
+    }
+
+    findAll() {
+        return this.prisma.category.findMany()
+    }
+
+    async findOne(id: number){
+        const category = await this.prisma.category.findUnique({
+            where: { id },
+        })
+
+        if(!category) {
+            throw new NotFoundException(`Categoria com ID ${id} não encontrada`)
+        }
+
+        return category
+    }
+
+    async update(id: number, UpdateCategoryDto: UpdateCategoryDto) {
+        try{            
+            const category = await this.prisma.category.update({
+                where: { id },
+                data: UpdateCategoryDto,                
+            })
+        }catch(error){
+            throw new NotFoundException(`Categoria com ID ${id} não encontrada`)
+        }
+    }
+
+    async remove(id: number){
+        try{
+            return this.prisma.category.delete({
+                where: {id}
+            })
+        }catch(error){
+            throw new NotFoundException(`Categoria com o ID ${id} não encontrada`)
+        }
+    }
+}
--- src/pizzas/pizzas.service.ts
+++ src/pizzas/pizzas.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePizzaDto } from './dto/create-pizza.dto';
+import { UpdatePizzaDto } from './dto/update-pizza.dto';
 
 @Injectable()
-export class PizzasService {}
+export class PizzasService {
+    constructor(private prisma: PrismaService){}
+
+    async create(createPizzaDto: CreatePizzaDto) {
+        return this.prisma.pizza.create({
+            data: createPizzaDto,
+        })
+    }
+
+    findAll() {
+        return this.prisma.pizza.findMany()
+    }
+
+    async findOne(id: number){
+        const pizza = await this.prisma.pizza.findUnique({
+            where: {id},
+        })
+
+        if(!pizza) {
+            throw new NotFoundException(`Pizza com o ID ${id} não encontrada`)
+        }
+
+        return pizza
+    }
+
+    async update (id: number, UpdatePizzaDto: UpdatePizzaDto) {
+        try{
+            const pizza = await this.prisma.pizza.update({
+                where: {id},
+                data: UpdatePizzaDto,
+            })
+        }catch(error){
+            throw new NotFoundException(`Pizza com o ID ${id} não encontrada`)
+        }
+    }
+
+    async remove(id: number){
+        try{
+            return this.prisma.pizza.delete({
+                where: {id}
+            })
+        }catch(error){
+            throw new NotFoundException(`Categoru com o ID ${id} não encontrada`)
+        }
+    }
+}
